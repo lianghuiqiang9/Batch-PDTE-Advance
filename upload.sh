a62c476d1fee360359a349e3d12c8d854ca0e3b8
lianghuiqiang9 <lianghuiqiang92@gmail.com>
2025-06-24 02:55:30 +0000

    change-readme.txt-2025-06-24

--- upload.sh
+++ upload.sh
@@ -2,5 +2,5 @@ echo "the running file: $0"
 commit_log=$1
 current_date=$(date +'%Y-%m-%d')
 git add .
-git commit -m "$current_date+$commit_log"
+git commit -m "$commit_log-$current_date"
 git push origin main
